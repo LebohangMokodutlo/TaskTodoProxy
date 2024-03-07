95a07aae70154a6ae4ff5fed93ee4a5a65da4b3e
Lebohang <lebohang@afrigis.co.za>
2024-03-07 14:26:45 +0200

    updated the database

--- TaskTodoProxy/Entities/TaskTodo.cs
+++ TaskTodoProxy/Entities/TaskTodo.cs
@@ -9,15 +9,10 @@ namespace TaskTodoProxy.Entities
         public string Department { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; }
-
-        public DateTime Deadline { get; set; }
-
+        public DateOnly Deadline { get; set; }
         public string Priority { get; set; }
-
         public string TaskStatus { get; set; }
-
         public string ClientUrl { get; set; }
-
         public string ClientBudget { get; set; }
 
     }
